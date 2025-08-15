ccdf5514a2147d07edff22c1cd16572fe85d2ec8
SOM0027 <som0027@bssc.edu.au>
2025-08-15 10:44:24 +1000

    added stuff idk cant remember

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Date: 14-8-2025
 from flask import Flask, render_template, request
 
-items={}
+items = {}
 
 app = Flask(__name__) 
 
@@ -10,27 +10,31 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html', password="lost")
+
 @app.route("/find")
 def find():
-    return render_template('find.html')
+    print(items)
+    return render_template('find.html', items=items)
 
 @app.route("/find", methods=['POST'])
 def find_item():
     print(request.form)
 
-@app.route('/report', methods=['POST'])
-def add_item():
-    print(request.form)
-    items[request.form['description']] = {
-        "author": request.form['name'],
-        "question": request.form['question'],
-        "answer": request.form['answer']
-    }
-    return "Your item has been added. <br><a href='/'>Return to main page</a>"
-
 @app.route('/report')
 def report():
     return render_template('report.html')
+
+@app.route('/report', methods=['POST'])
+def add_item():
+    global items
+    items[request.form["item"]] = {
+        "name": request.form["name"],
+        "colour": request.form["select"]
+    }
+    return "Your item has been added. <br><a href='/'>Return to main page</a>"
 
 if __name__ == '__main__':
     app.run(debug=True)
